852f550416d2db8a0365bebfd10004e31a4e8618
Maxwell <star32975@gmail.com>
2021-03-22 21:23:11 +0200

    Added bussines layer, Wrote interfaces, become implement them in EFDirectory / EFMaterial repositories.

---
+++ BussinesLayer/Interfaces/IDirectoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataLayer.Entities;
+
+namespace BussinesLayer.Interfaces
+{
+    public interface IDirectoryRepository
+    {
+        IEnumerable<Directory> GetAllDirectories(bool includeMaterials = false);
+
+        Directory GetDirectoryById(int directoryId, bool includeMaterials = false);
+        void SaveDirectory(Directory achieve);
+        void DeleteDirectory(Directory achieve);
+    }
+}
---
+++ BussinesLayer/Interfaces/IMaterialsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataLayer.Entities;
+
+namespace BussinesLayer.Interfaces
+{
+    public interface IMaterialsRepository
+    {
+        IEnumerable<Material> GetAllDirectories(bool includeDirectory = false);
+
+        Material GetMaterialById(int directoryId, bool includeDirectory = false);
+        void SaveMaterial(Material mateiral);
+        void DeleteMaterial(Material mateiral);
+    }
+}
