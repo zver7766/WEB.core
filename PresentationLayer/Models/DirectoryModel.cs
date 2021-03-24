4858fd086367207e1020a580867e9edd9699a1c7
Maxwell <star32975@gmail.com>
2021-03-24 19:40:00 +0200

    Added presentation layer, some models and serivce.

---
+++ PresentationLayer/Models/DirectoryModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Entities;
+
+namespace PresentationLayer.Models
+{
+    public class DirectoryViewModel : PageViewModel
+    {
+        public Directory Directory { get; set; }
+        public List<MaterialViewModel> Materials { get; set; }
+
+    }
+
+    public class DirectoryEditModel : PageEditModel
+    {
+
+    }
+}
---
+++ PresentationLayer/Models/MaterialModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataLayer.Entities;
+
+namespace PresentationLayer.Models
+{
+    public class MaterialViewModel : PageViewModel
+    {
+        public Material Material { get; set; }
+        public Material NextMaterial { get; set; }
+    }
+
+    public class MaterialEditModel : PageEditModel
+    {
+        [Required]
+        public int DirectoryId { get; set; }
+    }
+}
